23b052bcddbf7d8284b63c1b0ae9c070dd443eff
zhengdongqi <zhengdongqi@huohua.cn>
2020-12-20 21:52:30 +0800

    虚函数表

--- 02.C++/07.virtual_table.cpp
+++ 02.C++/07.virtual_table.cpp
@@ -9,25 +9,50 @@
 #include <algorithm>
 using namespace std;
 
-class A {
+class Animal {
 public :
-    virtual void run(int x) {
-        cout << "class A run" << endl;
+    Animal() {
+        x = 8827, y = 65123;
     }
+    virtual void say(int x) {
+        cout << "I don't know how to say" << endl;
+    }
+    virtual void run() {
+        cout << "I don't know how to run" << endl;
+    }
+protected :
+    int x, y;
 };
-class B : public A {
+
+class Cat : public Animal {
 public :
-    void run(int x) {
-        cout << "class B run" << endl;
+    void say(int x) override {
         cout << this << endl;
+        cout << this->x  << " " << this->y << endl;
         cout << x << endl;
+        cout << "miao~ miao~ miao~" << endl;
+    }
+    void run() override {
+        cout << "I can run with four legs" << endl;
     }
 };
 
-typedef void (*func)(void *, int);
+void output_raw_data(void *q, int n) {
+    printf("%p : ", q);
+    unsigned char *p = (unsigned char *)q;
+    for (int i = 0; i < n; i++) {
+        printf("%02X ", p[i]);
+    }
+    printf("\n");
+    return ;
+}
+
+typedef void (*func)(void *, int x);
 
 int main() {
-    B b;
-    ((func **)(&b))[0][0](&b, 123);
+    Cat a, b;
+    output_raw_data(&a, sizeof(a));
+    output_raw_data(&b, sizeof(b));
+    ((func **)(&a))[0][0](&a, 123);
     return 0;
 }
